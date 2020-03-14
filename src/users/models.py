681469cda2cc5899a6e82402cae4be5de429001f
Max Rosenthal <mxrosenthal@yahoo.com>
2020-03-14 14:54:52 -0700

    updated user model to use charfield instead of text field for photo_url

--- src/users/models.py
+++ src/users/models.py
@@ -5,6 +5,6 @@ class User(models.Model):
   first_name = models.CharField(max_length=100)
   last_name = models.CharField(max_length=100)
   email = models.CharField(max_length=100)
-  password = models.CharField(max_length=100)
-  photo_url = models.TextField()
+  password = models.CharField(max_length=100)  
+  photo_url = models.CharField(max_length=100, blank=True, null=True)
   bio = models.TextField(blank=True, null=True)
