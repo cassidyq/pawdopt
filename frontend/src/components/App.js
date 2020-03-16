54618390767576d1efc2ba982eaa832d15b2fad9
BenjaminVincent <ben.tom.vincent@gmail.com>
2020-03-16 09:41:59 -0700

    working on basic Navbar

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import '../styles/App.css';
+import Navbar from './Navbar'
 
 class App extends Component {
   state = {
@@ -44,6 +45,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <Navbar/>
         {this.state.users.map(item => (
           <div key={item.id}>
             <h1>{item.first_name}</h1>
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../styles/Navbar.css';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div>Navbar</div>
+    );
+  }
+}
+
+
+export default Navbar;
