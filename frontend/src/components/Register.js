d2407375220a81796c76ca463b3b2953c7da3d5c
Max Rosenthal <mxrosenthal@yahoo.com>
2020-03-18 13:47:36 -0700

    made intermediate page to select whether you are registering as a user or organization.

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,16 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import '../styles/base-padding.scss';
+import { Button, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+import './Login.css';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 
-class Register extends Component {
-  render() {
-    return (
-      <body>
-        <p>
-          Register
-        </p>
-      </body>
-    )
+export default function Login(props) {
+  function handleSubmit(event) {
+    event.preventDefault();
   }
+
+  return (
+    <div className='RegisterAs'>
+      <Link to='/RegisterUser' className='register-user'>
+        <Button block bsSize='large' type='submit'>
+          User
+        </Button>
+      </Link>
+      <Link to='/RegisterShelter' className='register-shelter'>
+        <Button block bsSize='large' type='submit'>
+          Organization
+        </Button>
+      </Link>
+    </div>
+  );
 }
 
-export default Register;
+// export default Register;
