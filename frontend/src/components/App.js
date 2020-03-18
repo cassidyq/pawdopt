f3a58fbe7ce3b2f7e7a1a6c684528c1071c25714
Max Rosenthal <mxrosenthal@yahoo.com>
2020-03-18 13:07:45 -0700

    created login form. Hitting submit returns a user to the home page. Still need to create cookie and permissions.

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,23 +3,20 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './Home';
 import About from './About';
-import Login from './Login';
 import Register from './Register';
 import Shelter from './Shelter';
-
-
+import Login from './Login';
 
 class App extends Component {
-  
   render() {
     return (
       <BrowserRouter>
         <Navbar />
-        <Route exact path ="/" component={Home}/>
-        <Route exact path ="/about" component={About}/>
-        <Route exact path ="/login" component={Login}/>
-        <Route exact path ="/register" component={Register}/>
-        <Route exact path ="/shelter" component={Shelter}/>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/about' component={About} />
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/shelter' component={Shelter} />
+        <Route exact path='/login' exact component={Login} />
       </BrowserRouter>
     );
   }
