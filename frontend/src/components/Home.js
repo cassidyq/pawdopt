fdb1a89d199c15d8f6876e08660d56d25cd68dce
Max Rosenthal <mxrosenthal@yahoo.com>
2020-03-18 13:22:25 -0700

    pulling in animals to homepage.

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,16 +3,16 @@ import '../styles/base-padding.scss';
 
 class Home extends Component {
   state = {
-    adopters: []
+    animals: []
   };
 
   async componentDidMount() {
     try {
-      const res = await fetch('http://127.0.0.1:8000/api/adopters'); // fetching the data from api, before the page loaded
-      const adopters = await res.json();
+      const res = await fetch('http://127.0.0.1:8000/api/animals'); // fetching the data from api, before the page loaded
+      const animals = await res.json();
       console.log('testing');
       this.setState({
-        adopters
+        animals
       });
     } catch (e) {
       console.log(e);
@@ -23,7 +23,7 @@ class Home extends Component {
     return (
       <body>
         <p>Welcome to Pawdopt!</p>
-        {this.state.adopters.map(item => (
+        {this.state.animals.map(item => (
           <div key={item.id}>
             <h1>{item.name}</h1>
           </div>
