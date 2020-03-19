b131a9e850935ad387c30da669af6cb8f2e95ed1
Max Rosenthal <mxrosenthal@yahoo.com>
2020-03-19 10:07:32 -0700

    added logout route witch invalidates a user's token.

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
-from .api import RegisterAPI, LoginAPI
+from .api import RegisterAPI, LoginAPI, UserAPI
 from knox import views as knox_views
 
 urlpatterns = [
   path('api/auth', include('knox.urls')),
   path('api/auth/register', RegisterAPI.as_view()),
   path('api/auth/login', LoginAPI.as_view()),
+  path('api/auth/user', UserAPI.as_view()),
 
+  #invalidates the token, so people will have to log in again to access their page.
+  path('api/auth/logout', knox_views.LogoutView.as_view(), name='knox_logout')
 ]
