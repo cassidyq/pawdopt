1ebdf222a83300897e6729706a05f8d2e16153f0
BenjaminVincent <ben.tom.vincent@gmail.com>
2020-03-20 09:51:53 -0700

    updated filter in backend for case insensitive

--- src/animals/views.py
+++ src/animals/views.py
@@ -34,13 +34,13 @@ class FilterAnimals(generics.ListAPIView):
         shelter_id = self.request.query_params.get('shelter_id', None)
         # refactor later -> use loop
         if animal_type is not None:
-            queryset = queryset.filter(animal_type=animal_type)
+            queryset = queryset.filter(animal_type__iexact=animal_type)
         if breed is not None:
-            queryset = queryset.filter(breed=breed)
+            queryset = queryset.filter(breed__iexact=breed)
         if age is not None:
-            queryset = queryset.filter(age=age)
+            queryset = queryset.filter(age__iexact=age)
         if gender is not None:
-            queryset = queryset.filter(gender=gender)
+            queryset = queryset.filter(gender__iexact=gender)
         if shelter_id is not None:
-            queryset = queryset.filter(shelter_id=shelter_id)
+            queryset = queryset.filter(shelter_id__iexact=shelter_id)
         return queryset
