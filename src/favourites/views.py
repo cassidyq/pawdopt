247749e59b77cde11b77fc5bacaded4b8ffc0e9f
Max Rosenthal <mxrosenthal@yahoo.com>
2020-03-26 22:52:07 -0700

    cleaned up the code in the query for favourited animals.

--- src/favourites/views.py
+++ src/favourites/views.py
@@ -18,29 +18,6 @@ class DetailFavourites(generics.RetrieveUpdateDestroyAPIView):
     
 class UserFavourites(APIView):
     def get(self, request, userID):
-        print(self)
-        print(userID)
-        print(request)
-        # queryset = Animal.objects.all()
-        # userID = userID
-        # my_query = Favourite.objects.filter(favourite__user_id=userID)
         animalsWeWant = Favourite.objects.filter(user_id = userID).values_list('animal_id', flat=True) #values_list('user_id', flat=True)
         my_query = Animal.objects.filter(pk__in=animalsWeWant).values('name', 'description', 'photo_url', 'animal_type', 'breed', 'age', 'size', 'gender', 'shelter_id')
-        print(my_query)
         return Response(my_query)
-        # return HttpResponse(my_query, content_type="application/json")
-
-        # return Response(my_query)
-
-
-
-
-    # userID = userID
-    # # something = Favourite.objects.filter(id = userID)
-    # context = {'favourited_animals': my_query}
-
-    # # return render (request, context)
-    # return Response({
-    #     "animal": AnimalSerializer(animal, context=self.get_serializer_context()).data,
-    #     "hey":"big guy",
-    # })
